419b09b0d2dfa04c66a6eb91d3c24a64e1175680
Sudeepktiwari <sudeep@appointy.com>
2025-01-16 10:47:53 +0530

    favicon-and-text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import TopBar from "./components/top-bar/top-bar";
 import Homepage from "./pages/home/homepage";
+import { useEffect } from "react";
+import logo from "./assets/logo.png";
 
 function App() {
+  useEffect(() => {
+    const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+    if (link) {
+      link.href = logo; // Replace with your icon path
+
+      document.title = "Home - My Custom App";
+    }
+  }, []);
+
   return (
     <div className="bg-gray-100 flex flex-col min-h-screen">
       <div>
