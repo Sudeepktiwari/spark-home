e1a35f41429af084bc058e2603edc048fc9581fe
Sudeepktiwari <sudeep@appointy.com>
2025-01-25 15:55:37 +0530

    introfontfix

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -28,7 +28,7 @@ const Intro = () => {
       lg:bg-[size:98%_130%] bg-[size:100%_80%] bg-no-repeat bg-right-bottom lg:bg-center
       lg:gap-2 lg:py-20 lg:flex items-center "
       >
-        <div className="lg:pl-44 lg:w-[40vw] lg:relative lg:bottom-8">
+        <div className="lg:pl-44 lg:w-[40vw] relative bottom-14">
           <p
             style={{ fontFamily: "Roboto" }}
             className=" text-center lg:text-start text-2xl lg:text-[2rem] 2xl:text-[2.5rem] 
@@ -42,7 +42,7 @@ const Intro = () => {
           </p>
           <p
             style={{ fontFamily: "Open Sans" }}
-            className="text-xs text-center py-4 lg:py-0 lg:text-start lg:text-sm 2xl:text-xl font-light "
+            className="text-xs text-center py-4 pt-8 lg:text-start lg:text-sm 2xl:text-xl font-light "
           >
             The AI Chatbot You Can{" "}
             <span className="font-semibold underline underline-offset-4 text-orange-500">
@@ -50,6 +50,15 @@ const Intro = () => {
             </span>
             —Easier and Faster Than Any Other!
           </p>
+          <div className=" gap-8 items-center flex lg:w-[30vw] pt-4">
+            <Button className=" bg-[#5555ff] hover:bg-white hover:text-black lg:w-[16vw] ">
+              Sign Up Free
+            </Button>
+            <div className="flex flex-col items-center text-center ">
+              <StarRating />
+              <p>Based on 10,000+ reviews</p>
+            </div>
+          </div>
         </div>
         <div className="lg:w-[10vw] lg:h-[60vh] relative lg:left-16 lg:bottom-6">
           <Lottie
@@ -67,13 +76,13 @@ const Intro = () => {
         <img src={imglightbg} alt="Background" />
         </div> */}
       </div>
-      <div className="lg:flex items-center py-10">
-        <p className="flex items-center text-justify px-4">
+      <div className="lg:flex items-center  py-10">
+        <p className="flex items-center text-lg text-center px-20">
           Quickly Build Smart AI Chatbots to Scale Your Support and Boost Sales.
           SparkAgentAI automates interactions for customer support, lead
           qualification, and more—delivering instant value from day one.
         </p>
-        <div className=" justify-center gap-4 items-center flex lg:w-[50vw]">
+        {/* <div className=" justify-center gap-4 items-center flex lg:w-[50vw]">
           <Button className=" bg-[#5555ff] hover:bg-white hover:text-black lg:w-[20vw] ">
             Sign Up Free
           </Button>
@@ -81,7 +90,7 @@ const Intro = () => {
             <StarRating />
             <p>Based on 10,000+ reviews</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
