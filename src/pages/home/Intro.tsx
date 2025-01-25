fd8940a156bc33bd12c4fd9e3ab48efdb52a22f7
Sudeepktiwari <sudeep@appointy.com>
2025-01-25 17:03:28 +0530

    phonefix

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -26,9 +26,9 @@ const Intro = () => {
         // bg-[size:60%_50%]
         className="
       lg:bg-[size:98%_130%] bg-[size:100%_80%] bg-no-repeat bg-right-bottom lg:bg-center
-      lg:gap-2 lg:py-20 lg:flex items-center bg-black shadow-2xl "
+      lg:gap-2 pb-24 lg:pt-20 lg:pb-20 lg:flex items-center bg-black shadow-2xl "
       >
-        <div className="lg:pl-44 lg:w-[40vw] relative bottom-14 ">
+        <div className="lg:pl-44 lg:w-[40vw] lg:relative lg:bottom-14 ">
           <p
             style={{ fontFamily: "Roboto" }}
             className=" text-center text-white lg:text-start text-2xl lg:text-[2rem] 2xl:text-[2.5rem] 
@@ -50,7 +50,7 @@ const Intro = () => {
             </span>
             —Easier and Faster Than Any Other!
           </p>
-          <div className=" gap-8 items-center flex lg:w-[30vw] pt-4">
+          <div className=" gap-8 items-center hidden lg:flex lg:w-[30vw] pt-4">
             <Button
               className=" bg-[#5555ff] hover:bg-white hover:text-black text-[24px] 
             lg:w-[16vw] rounded-full py-8 "
@@ -68,7 +68,7 @@ const Intro = () => {
             autoplay
             loop
             animationData={introAnim}
-            className="lg:w-[50vw] lg:h-[60vh] w-[90vw] py-4 px-4"
+            className="lg:w-[50vw] lg:h-[60vh] w-[90vw] h-[30vh] py-4 px-4 "
           />
         </div>
 
@@ -79,21 +79,21 @@ const Intro = () => {
         <img src={imglightbg} alt="Background" />
         </div> */}
       </div>
-      <div className="lg:flex items-center  py-10 bg-black">
-        <p className="flex items-center text-lg text-center px-20 text-white ">
+      <div className="lg:flex items-center py-10 bg-black">
+        <p className="flex items-center text-lg text-justify lg:text-center px-4 lg:px-20 text-white ">
           Quickly Build Smart AI Chatbots to Scale Your Support and Boost Sales.
           SparkAgentAI automates interactions for customer support, lead
           qualification, and more—delivering instant value from day one.
         </p>
-        {/* <div className=" justify-center gap-4 items-center flex lg:w-[50vw]">
+        <div className=" justify-center gap-4 items-center flex pt-10 lg:pt-0 lg:w-[50vw]">
           <Button className=" bg-[#5555ff] hover:bg-white hover:text-black lg:w-[20vw] ">
             Sign Up Free
           </Button>
-          <div className="flex flex-col items-center text-center w-[20vw]">
+          <div className="flex flex-col items-center text-center text-white">
             <StarRating />
             <p>Based on 10,000+ reviews</p>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
