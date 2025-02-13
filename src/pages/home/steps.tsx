4c0da23e0c406130bc3693248fe3fa54174dd4d8
Sudeepktiwari <sudeep@appointy.com>
2025-02-13 07:54:46 +0530

    steps-bg-fix

--- src/pages/home/steps.tsx
+++ src/pages/home/steps.tsx
@@ -7,12 +7,7 @@ import stepsAnim from "../../assets/NewImages/3 Steps.json";
 
 const Steps = () => {
   return (
-    <div
-      className="px-4
-      bg-gradient-to-b from-white 
-      via-[#fefef2] to-[#fefef2] py-10
-    "
-    >
+    <div className="px-4 bg-white">
       <h1
         className="text-center text-2xl lg:text-5xl lg:p-16 "
         style={{ fontWeight: 300 }}
